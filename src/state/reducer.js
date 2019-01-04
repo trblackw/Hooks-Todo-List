730443ab953683576991e0cec075db1858f7187d
Tucker <trblackw@ncsu.edu>
2019-01-04 10:36:38 -0500

    added toggle reducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,10 +1,22 @@
+import { TOGGLE_TODO } from "../components/TodoList";
+
 const TodosReducer = (state = {}, action) => {
-   const { type } = action;
-   switch (type) {
-     default:
-       return state;
-   }
- };
- 
- export default TodosReducer;
- 
+  const { type, todo: selectedTodo } = action;
+  const { todos } = state;
+  switch (type) {
+    case TOGGLE_TODO:
+      const toggledTodos = todos.map(todo =>
+        todo.id === selectedTodo.id
+          ? { ...selectedTodo, complete: !selectedTodo.complete }
+          : todo
+      );
+      return {
+        ...state,
+        todos: toggledTodos
+      };
+    default:
+      return state;
+  }
+};
+
+export default TodosReducer;
