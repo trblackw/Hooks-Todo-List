e415c83b87faf73aef8e5de69e040d3d2841cabb
Tucker <trblackw@ncsu.edu>
2019-01-04 08:22:40 -0500

    restructured app and app state to use useContext and useReducer hooks

--- src/App.js
+++ src/App.js
@@ -1,67 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Todo from "./Todo";
-import TodoForm from "./TodoForm";
+import React, { useContext, useReducer } from "react";
+import TodosContext from "./state/context";
+import TodosReducer from "./state/reducer";
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      text: "Learn Hooks",
-      isCompleted: false
-    },
-    {
-      text: "Get some sushi",
-      isCompleted: false
-    },
-    {
-      text: "Meet Chris",
-      isCompleted: false
-    }
-  ]);
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-  const deleteTodo = todo => {
-    const newTodos = todos.filter(
-      item => item.text.toLowerCase() !== todo.text.toLowerCase()
-    );
-    setTodos(newTodos);
-  };
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
-  };
-  const editTodo = (index, value) => {
-    const newTodos = [...todos];
-    if (value !== "") {
-      newTodos[index].text = value;
-      console.log(newTodos[index]);
-    }
-    setTodos(newTodos);
-  };
-
-  return (
-    <div className="app">
-      <TodoForm addTodo={addTodo} />
-      <div className="todo-list">
-        {todos.length > 0 ? (
-          todos.map((todo, index) => (
-            <Todo
-              key={index}
-              index={index}
-              todo={todo}
-              remove={deleteTodo}
-              complete={completeTodo}
-              edit={editTodo}
-            />
-          ))
-        ) : (
-          <code>todos go here</code>
-        )}
-      </div>
-    </div>
-  );
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(TodosReducer, initialState);
+   return (
+      <TodosContext.Provider value={{state, dispatch}}>
+         
+     </TodosContext.Provider>
+  )
 };
 
 export default App;
---
+++ src/state/reducer.js
@@ -0,0 +1,10 @@
+const TodosReducer = (state = {}, action) => {
+   const { type } = action;
+   switch (type) {
+     default:
+       return state;
+   }
+ };
+ 
+ export default TodosReducer;
+ 
