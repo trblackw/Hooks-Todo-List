da3e7764a2ff82e0aca1c7624557b16f819502b5
Tucker <trblackw@ncsu.edu>
2019-01-04 08:34:18 -0500

    destructured todos from state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useContext, useReducer } from "react";
 import TodosContext from "./state/context";
 import TodosReducer from "./state/reducer";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   const initialState = useContext(TodosContext);
-  const [state, dispatch] = useReducer(TodosReducer, initialState);
-   return (
-      <TodosContext.Provider value={{state, dispatch}}>
-         
-     </TodosContext.Provider>
-  )
+  const [{ todos }, dispatch] = useReducer(TodosReducer, initialState);
+  return (
+    <TodosContext.Provider value={{ todos, dispatch }}>
+      <TodoList />
+    </TodosContext.Provider>
+  );
 };
 
 export default App;
