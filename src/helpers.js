9e1049da67d84c805fc9960597fab6b8df42e2c0
Tucker <trblackw@ncsu.edu>
2019-01-05 20:28:28 -0500

    wrote time stamp helper for todo time stamps

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+export const generateTimeStamp = () => {
+  const timestamp = Date.now();
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  }).format(timestamp);
+  return formattedTime;
+};
